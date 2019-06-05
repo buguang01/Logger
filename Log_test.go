e147f270675513e4da2d8ad1d1d2df533edde3e3
buguang01 <273033431@qq.com>
2019-06-05 13:40:23 +0800

    mod up

--- Log_test.go
+++ Log_test.go
@@ -1,30 +1,28 @@
-package Logger_test
+package Logger
 
 import (
 	"testing"
-
-	"github.com/buguang01/Logger"
 )
 
 func TestLog(t *testing.T) {
 	//正常退出日志模块
-	defer Logger.LogClose()
+	defer LogClose()
 
 	defer func() {
 		r := recover()
 		if r != nil {
-			Logger.PFatal(r)
+			PFatal(r)
 
 		}
 	}()
 	//初始化日志模块
-	Logger.Init(0, "logs")
-	Logger.SetListenKeyID(1001)
-	Logger.PDebug("test1")
-	Logger.PInfo("test2")
-	Logger.PInfoKey("test3", 1001)
-	Logger.PDebugKey("test4", 1002)
-	Logger.PDebugKey("test5", 1001)
+	Init(0, "logs")
+	SetListenKeyID(1001)
+	PDebug("test1")
+	PInfo("test2")
+	PInfoKey("test3", 1001)
+	PDebugKey("test4", 1002)
+	PDebugKey("test5", 1001)
 	//errs := fmt.Errorf(string(debug.Stack()))
 	panic("panicinfo")
 
